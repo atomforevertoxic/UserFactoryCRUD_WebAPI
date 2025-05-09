66aeb45dceb2bf18707e29d67a91bba82acb49b0
Shatalov Ivan <139710109+atomforevertoxic@users.noreply.github.com>
2025-05-09 17:07:34 +0300

    Implemented GET methods in UsersController

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserFactory.Data;
+
+namespace UserFactory.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly WebDbContext _context;
+
+        public UsersController(WebDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserByGuid(Guid guid)
+        {
+            var user = await _context.Users.FindAsync(guid);
+            if (user==null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
