bd027ad5acceed048ce5bf579d7b5a9d5bc9528a
Shatalov Ivan <139710109+atomforevertoxic@users.noreply.github.com>
2025-05-14 18:14:54 +0300

    [Fix] User model nullable variables fixed

--- Models/User.cs
+++ Models/User.cs
@@ -5,22 +5,22 @@ namespace UserFactory.Models
 {
     public class User
     {
-        public Guid Id { get; set; } = Guid.NewGuid(); 
+        public Guid Id { get; set; } = Guid.NewGuid();
 
         [Required]
         [RegularExpression("^[A-Za-z0-9]+$", ErrorMessage = "Login can contain only Latin letters and numbers")]
         [StringLength(30, MinimumLength = 3, ErrorMessage = "Login must be between 3 and 30 characters")]
-        public string Login { get; set; }
+        public string Login { get; set; } = default!;
 
         [Required]
         [RegularExpression("^[A-Za-z0-9]+$", ErrorMessage = "Password can contain only Latin letters and numbers")]
         [StringLength(30, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
-        public string Password { get; set; }
+        public string Password { get; set; } = default!;
 
         [Required]
         [RegularExpression("^[A-Za-zА-Яа-яЁё]+$", ErrorMessage = "The name can only contain Latin and Russian letters")]
         [StringLength(30, ErrorMessage = "Name cannot be longer than 30 characters")]
-        public string Name { get; set; }
+        public string Name { get; set; } = default!;
 
         [Required(ErrorMessage = "The Gender field is required.")]
         public int? Gender { get; set; }
@@ -28,7 +28,7 @@ namespace UserFactory.Models
         [DataType(DataType.Date)]
         public DateTime? Birthday { get; set; }
 
-        public bool Admin { get; set; } = true;
+        public bool Admin { get; set; } = false;
 
         public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
         public string? CreatedBy { get; set; }
